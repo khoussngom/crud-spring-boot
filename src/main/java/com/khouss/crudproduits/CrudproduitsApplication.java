dc448f5bda12f8da46448e6468a690775a6f2cee
khoussngom <khoussn@gmail.com>
2024-12-09 15:16:58 +0000

    first commit

---
+++ src/main/java/com/khouss/crudproduits/CrudproduitsApplication.java
@@ -0,0 +1,13 @@
+package com.khouss.crudproduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudproduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudproduitsApplication.class, args);
+	}
+
+}
